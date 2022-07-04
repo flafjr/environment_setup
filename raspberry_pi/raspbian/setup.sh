a133b55d2557d5ab12cb7e5f17116bdddb66ebfb
Flavio Ferreira <flafjr@gmail.com>
2022-07-03 21:44:52 -0600

    PI setup overall improvements when setting up git

--- raspberry_pi/raspbian/setup.sh
+++ raspberry_pi/raspbian/setup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+ANSIBLE_FOUND="$(command -v ansible)"
+OS="$(uname)" #TODO Improve
+
+if [ "$OS" != "Linux" ];
+then
+  echo "You are not using a Pi, please check if we have the automation ready for your OS $OSTYPE"
+  exit 1
+fi
+
+if [ -z "$ANSIBLE_FOUND" ]
+then
+  echo "ANSIBLE NOT FOUND"
+  pip3 install --upgrade pip
+  sudo pip3 install ansible
+else
+  echo "ANSIBLE FOUND: $ANSIBLE_FOUND"
+fi
 
 ansible-galaxy install -r requirements.yml
 ansible-playbook -i hosts.ini main.yml
